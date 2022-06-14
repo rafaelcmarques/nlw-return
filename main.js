93d8bf09f8ba3927e07b5832eefa4af73844d7c7
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2022-06-13 23:26:03 -0300

    Adicionado a função activateMenuAtCurrentSection

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
-  activateMenuAtCurrentSection()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
 function showNavOnScroll() {
@@ -14,7 +18,7 @@ function showNavOnScroll() {
   }
 }
 
-function activateMenuAtCurrentSection() {
+function activateMenuAtCurrentSection(section) {
   //linha alvo
   //scrollY = posição da barra de rolagem na pagina  innerHeight = campo de visao, visivel em uma pagina
   const targetLine = scrollY + innerHeight / 2
@@ -22,9 +26,33 @@ function activateMenuAtCurrentSection() {
   //verificar se a seção passou da linha
   //quais dados vou precisar?
 
-  const sectionTop = home.offsetTop //topo da pagina
+  //topo da pagina
+  const sectionTop = section.offsetTop
 
+  //altura da pagina
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha?
   const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  //console.log('Topo passou da linha?' + sectionTopReachOrPassedTargetLine)
+
+  //a seção termina a onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //o final da seção passou da linha?
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+  //console.log(' O fim passou da linha? ' + sectionEndPassedTargetLine)
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}`)
+  menuElement.classList.remove('active')
+
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showBackToTopButtonOnScroll() {
